a8a0f53d0166e56f23d4f4d1d4a26a4bd33a09e9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 13:50:08 -0800

    B trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new == NULL)
 		return (NULL);
 
-	new->value = value;
+	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
