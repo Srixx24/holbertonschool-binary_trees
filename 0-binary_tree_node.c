e8ceb4a5a2feb58fd14efb3a6e4588d4ba29a933
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 13:46:03 -0800

    B trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,7 +6,7 @@
  * @value: value
  * Return: new node
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
