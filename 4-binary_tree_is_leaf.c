a40c4b337eb61fdcbebfc8471829b08139ed08ea
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 14:37:42 -0800

    B trees

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ * @node: node
+ * Return: 0 if null
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
+}
