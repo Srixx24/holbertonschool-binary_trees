5e682fda15734f1afc4878ecdb6597bba3b5a9d3
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 15:23:31 -0800

    B trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,24 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree)
+	{
+		size_t L = 0;
+		size_t R = 0;
+
+		L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((L > R) ? L : R);
+	}
+
+	return (0);
+}
