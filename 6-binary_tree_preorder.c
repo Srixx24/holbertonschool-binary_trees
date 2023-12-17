90e56b1717974c9c72583a9f34ace352d2e92e25
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 14:52:09 -0800

    B trees

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,1 +1,17 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_preorder - function that goes through a binary
+ * tree using pre-order traversal
+ * @tree: b tree
+ * @func: function to call node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
