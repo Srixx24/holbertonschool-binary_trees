ed54a66e584ccbd90d4017ff9ff0d82142b75ef0
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 20:50:46 -0800

    B trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_nodes - function that counts the nodes
+ * with at least 1 child in a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t R, L;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	R = binary_tree_nodes(tree->right);
+	L = binary_tree_nodes(tree->left);
+
+	return (1 + R + L);
+}
