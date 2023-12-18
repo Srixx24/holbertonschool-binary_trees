41987dc62a9dda5463e0e271faba1147d79de93d
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-18 08:10:47 -0800

    b trees

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_sibling - function that finds the sibling of a node
+ * @node: searching node
+ * Return: null if null
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent = node->parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (parent->left == node)
+		return (parent->right);
+	else
+		return (parent->left);
+}
