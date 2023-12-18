f8d7ff5e01cd5432a4d6c1b15c2ad98600541ce6
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 20:44:51 -0800

    B trees

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ *  binary_tree_leaves - function that counts the leaves
+ *  in a binary tree
+ *  @tree: b tree
+ *  Return: 0 if null
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t R, L;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	R = binary_tree_leaves(tree->right);
+	L = binary_tree_leaves(tree->left);
+
+	return (R + L);
+}
