101caf456aa03608855c6c6deeb85d973b783823
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 20:32:38 -0800

    B trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_size - function that measures the size
+ * of a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t R, L;
+
+	if (tree == NULL)
+		return (0);
+
+	R = binary_tree_size(tree->right);
+	L = binary_tree_size(tree->left);
+
+	return (1 + R + L);
+}
