c6735b2b559e9596eff835e3c5aaf017d3c29ce1
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-18 07:42:19 -0800

    b trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-size_t binary_tree_height(const binary_tree_t *tree);
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree)
+	{
+		size_t L = 0;
+		size_t R = 0;
+
+		L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((L > R) ? L : R);
+	}
+
+	return (0);
+}
 /**
  * binary_tree_is_perfect - function that checks if a
  * binary tree is perfect
