23fc280667504fdba70680057a6531f57f4c5998
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 21:37:23 -0800

    B trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,9 +12,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right != NULL) ||
-			(tree->left != NULL && tree->right == NULL))
-		return (0);
+	if ((tree->left == NULL && tree->right == NULL) || (tree->left && tree->right))
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	R = binary_tree_is_full(tree->right);
 	L = binary_tree_is_full(tree->left);
