6024ec501eca394e7fd80ee919ec03e4d1b110f9
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-18 08:34:07 -0800

    b trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree)
+	{
+		size_t L = 0;
+		size_t R = 0;
+
+		L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			return ((L > R) ? L : R);
+	}
+
+	return (0);
+}
+/**
  * binary_tree_balance - function that measures the
  * balance factor of a binary tree
  * @tree: b tree
@@ -13,3 +35,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	R = binary_tree_height(tree->right);
+	L = binary_tree_height(tree->left);
+
+	return(L - R);
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: searching node
+ * Return: null if null
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle = node->parent->parent;
+
+	if (node == NULL || node->uncle == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (uncle->left == node->parent)
+		return (uncle->right);
+	else
+		return (uncle->left);
+}
