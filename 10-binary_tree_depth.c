7ac450a1d93fcca0ae1629f2a10e03c63e854b1b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 20:19:13 -0800

    B trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_depth - function that measures the depth of
+ * a node in a binary tree
+ * @tree: b tree
+ * Return: d (depth)
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t d = 0;
+
+	if (tree == NULL)
+		return (0);
+	for (; tree->parent; d++)
+		tree + tree->parent;
+
+	return (d);
+}
