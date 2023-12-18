7c58ce0e5cd5115dd86caae04e6e76dee2097215
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-17 21:29:12 -0800

    B trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,1 +1,15 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_balance - function that measures the
+ * balance factor of a binary tree
+ * @tree: b tree
+ * Return: 0 if null
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t R, L;
+
+	if (tree == NULL)
+		return (0);
+
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,1 +1,23 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_is_full - function that checks if a binary tree is full
+ * @tree: b tree
+ * Return: 0 if null
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	size_t R, L;
+
+	if (tree == NULL)
+		reture (0);
+
+	if ((tree->left == NULL && tree->right != NULL) ||
+			(tree->left != NULL && tree->right == NULL))
+		return (0);
+
+	R = binary_tree_is_full(tree->right);
+	L = binary_tree_is_full(tree->left);
+
+	return (R && L);
+}
