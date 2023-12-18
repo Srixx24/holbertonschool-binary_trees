bb36921c19e91d500f49fa8d1083edcdf87bb48b
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-18 08:37:45 -0800

    b trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,8 +8,9 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *uncle = node->parent->parent;
+	binary_tree_t *parent = node->parent;
 
-	if (node == NULL || node->uncle == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	if (uncle->left == node->parent)
